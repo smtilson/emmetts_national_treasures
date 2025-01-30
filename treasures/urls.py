04c595cd24940e6157edf63ee9df8688ea16d3d5
seantilson <seantilson@gmail.com>
2025-01-30 13:48:48 +0100

    added a bit more from the rest framework. It seems that I need to add an api app and move some of the logic around.

--- treasures/urls.py
+++ treasures/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("treasure_list/", views.TreasureListCreate.as_view(), name="treasure_list"),
+    path("other_way/", views.get_data, name="other_way"),
+    path("other_way_add/", views.add_data, name="other_way_add"),
+    path("treasure_access/<int:pk>/", views.TreasureRetrieveUpdateDestroy.as_view(), name="treasure_access"),
 ]
