28934b12672d999cd906085c776f97ecb09e4673
seantilson <seantilson@gmail.com>
2025-02-05 17:59:36 +0100

    still doing tutorial, issue with onSelectItem not being a function, not worried since I am not following the tutorial super closely.

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -4,11 +4,25 @@ import Food from "./components/Food.jsx";
 import ListGroup from "./components/ListGroup.jsx";
 
 function App() {
+  let items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+  const handleSelectItem = (item) => {
+    console.log("handleSelectItem called. \nUser selected " + item);
+  };
   return (
     <>
       <Header />
-      <Food foods={["random", "randome"]} />
-      <ListGroup />
+      <ListGroup
+        items={["spaghetti", "randome"]}
+        heading={"Food but not really"}
+        onSelectItem={handleSelectItem}
+      />
+      <ListGroup items={items} heading={"Random item list"} />
       <Footer />
     </>
   );
--- test/src/components/ListGroup.jsx
+++ test/src/components/ListGroup.jsx
@@ -1,21 +1,34 @@
-import { Fragment } from "react";
+import { MouseEvent, useState } from "react";
 
-function ListGroup() {
-  let items = [
-    "An item",
-    "A second item",
-    "A third item",
-    "A fourth item",
-    "And a fifth one",
-  ];
-
+function ListGroup({ items, heading, onSelectItem }) {
+  //let items = props.items;
+  //let heading = props.heading;
+  // this useState is an example of a hook. It tells react that there may be state that changes.
+  // this setSelectedIndex function is an example of a hook. It just works!
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // this function is an example of a callback function. It is passed to the onClick event of the list items.
+  const handleClick = (e, item, index) => {
+    console.log("User clicked on " + item);
+    console.log("Event: ");
+    console.log(e);
+    setSelectedIndex(index);
+    onSelectItem(item);
+  };
   return (
     <>
-      <h1>List?</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items to display</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={(e) => handleClick(e, item, index)}
+          >
             {item}
           </li>
         ))}
