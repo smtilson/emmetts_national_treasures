765d18b77254f14b7ec9c5e414a7fbe4d616a1fb
smtilson <seantilson@gmail.com>
2025-02-13 12:10:32 +0100

    change treasures views to use view set instead of different methods or classes.

--- backend/treasures/api/views.py
+++ backend/treasures/api/views.py
@@ -1,42 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from ..models import Treasure
 from .serializers import TreasureSerializer
 # Create your views here.
 
 
-# there are other ways of doing this such as
-@api_view(["GET"])
-def get_data(request):
-    treasures = Treasure.objects.all()
-    serializer = TreasureSerializer(treasures, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["POST"])
-def add_data(request):
-    creator_id = request.user.id
-    data = request.data
-    data["creator_id"] = 1
-    serializer = TreasureSerializer(data=data)
-    print(data)
-    print(serializer.initial_data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    print(data)
-    return Response(serializer.errors)
-
-
-class TreasureListCreate(generics.ListCreateAPIView):
+class TreasureViewSet(viewsets.ModelViewSet):
     queryset = Treasure.objects.all()
     serializer_class = TreasureSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Treasure.objects.filter(creator=self.request.user)
 
-class TreasureRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Treasure.objects.all()
-    serializer_class = TreasureSerializer
-    lookup_field = "pk"
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
