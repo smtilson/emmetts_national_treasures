f5f865b7a1bdca18c81e7a94b823f5315775d367
smtilson <seantilson@gmail.com>
2025-02-13 11:09:07 +0100

    reorganize file structure, started working on api, closing this branch

---
+++ backend/comments/api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from ..models import Comment
+from .serializers import CommentSerializer
+
+
+# Create your views here.
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        return Comment.objects.filter(post=self.kwargs["post_pk"])
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
