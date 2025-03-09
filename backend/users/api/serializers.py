e5ee3a3040dc8f4f95ce0b6d72472d38671b2faa
smtilson <seantilson@gmail.com>
2025-03-09 21:57:27 +0100

    minor serializer added, in intial state.

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["id", "handle", "email", "date_joined", "last_login"]
+        fields = "__all__"
